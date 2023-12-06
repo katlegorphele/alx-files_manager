937f02ee5b19693e8f47f914fd8e40e4fa55c8db
Kat-Devs <katlegophele95@gmail.com>
2023-12-06 16:31:03 +0200

    task 0 - isAlive returning false on main.js

---
+++ utils/redis.js
@@ -0,0 +1,32 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+
+    this.client.on("error", (error) => {
+      console.log(`${error.message}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
+
+  async del(key) {
+    this.client.del(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
