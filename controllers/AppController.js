e1498be5648becdc3bf1d0f718babdb9df12b60c
Kat-Devs <katlegophele95@gmail.com>
2023-12-06 17:39:01 +0200

    utils/db.js update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,16 +5,16 @@ class AppController{
     static getStatus(req, res) {
         res
             .status(200)
-            .send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+            .json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
     }
 
     static async getStats(req, res) {
         try{
             const users = await dbClient.nbUsers();
             const files = await dbClient.nbFiles();
-            res.status(200).send({ users, files });
+            res.status(200).json({ users, files });
         } catch (err) {
-            res.status(500).send({ users: 0, files: 0 });
+            res.status(500).json({ err: err.message });
         }
     }
 }
--- utils/db.js
+++ utils/db.js
@@ -1,23 +1,22 @@
 import { MongoClient } from "mongodb";
 
+const HOST = process.env.DB_HOST || "localhost";
+const PORT = process.env.DB_PORT || 27017;
+const DB_NAME = process.env.DB_DATABASE || "files_manager";
+const URI = `mongodb://${HOST}:${PORT}`;
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 class DBClient {
   constructor() {
-    const HOST = process.env.DB_HOST || "localhost";
-    const PORT = process.env.DB_PORT || 27017;
-    const DB_NAME = process.env.DB_DATABASE || "files_manager";
-    const URI = `mongodb://${HOST}:${PORT}`;
-    const OPTIONS = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
     try {
       this.client = new MongoClient(URI, OPTIONS);
       this.client.connect();
       this.db = this.client.db(DB_NAME);
     } catch (error) {
       console.log(error);
-      this.client.close();
       this.db = false;
     }
   }
